59a4b10c9f2178bfdc85ed854b3e897024dd6f33
Kevin Chi <kevinjipotw@gmail.com>
2015-07-05 13:53:54 +0800

    scrol done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,6 +11,33 @@ $(document).ready(function() {
 	});
 
 	document.getElementById("aboutme").getElementsByTagName('h3')[0].click()
+
+	$("#aboutme").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#aboutme_scroll").offset().top
+	    }, 1500);
+	});
+
+	$("#resume").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#resume_scroll").offset().top
+	    }, 1500);
+	});
+
+	$("#projects").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#projects_scroll").offset().top
+	    }, 1500);
+	});
+
+
 });
+
+
+
+
+
+
+
 
 
